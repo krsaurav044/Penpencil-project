fc9254ee35a59c9334c16c898eb74016ebf6613d
krsaurav044 <32614792+krsaurav044@users.noreply.github.com>
2021-05-28 13:52:38 +0530

    Add files via upload

---
+++ src/components/PokeCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../appStyles.module.css'
+
+const PokeCard = ({pokemon}) => {
+    return (
+        <div className={styles.cardColumn} key={pokemon.id}>
+        <div className="card-header"><b>{pokemon.name}</b></div>
+        <div className="card-body">          
+          <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>  
+          <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>  
+          <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>  
+          <img src={pokemon.sprites['front_default']} />
+          <img src={pokemon.sprites['back_default']} />                       
+        </div>
+      </div>
+    )
+};
+
+export default PokeCard
